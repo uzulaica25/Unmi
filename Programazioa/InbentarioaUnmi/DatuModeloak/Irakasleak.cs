5f3d0af5cc70aed29827db9fa72f1d0379dee4eb
metxabe25 <metxabe25@izarraitz.eus>
2026-03-26 10:24:33 +0100

    Irakasleak klasea kodifikatuta

--- Programazioa/InbentarioaUnmi/DatuModeloak/Irakasleak.cs
+++ Programazioa/InbentarioaUnmi/DatuModeloak/Irakasleak.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace InbentarioaUnmi.DatuModeloak
 {
-    internal class Irakasleak
+    public class Irakasleak
     {
+        // Atributuak
+        private string izena;
+        private Mintegiak mintegia;
+        private bool minBurua;
+
+        //Get-Seterrak
+        public string Izena { get => izena; set => izena = value; }
+        public bool MinBurua { get => minBurua; set => minBurua = value; }
+        internal Mintegiak Mintegia { get => mintegia; set => mintegia = value; }
+
+        //Eraikitzailea
+        public Irakasleak(string i, Mintegiak m, bool mb)
+        {
+            this.izena = i;
+            this.mintegia = m;
+            this.minBurua = mb;
+        }
     }
 }
